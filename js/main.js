3c0856990f322dc26486128527ec22fa68908029
BarbarBetsy <barbarbetsy@googlemail.com>
2018-11-25 21:39:57 +0100

    turn card back after 2000

--- js/main.js
+++ js/main.js
@@ -3,13 +3,25 @@ function shuffle() {
     for (i = 1; i <= 16; i++) {
         let num = Math.floor(Math.random() * (100 - 1)) + 1;
         document.querySelector('#card' + i).style.order = num;
-        console.log('shuffled');
     }
 }
 
+function hideAgain() {
+$('.card').find('img').css('opacity', '0');
+}
+
+let counter = 0;
+
 // show card
-$('.card').click(function() {
-$(this).find('img').css('opacity', '1')
+$('.card').click(function () {
+    $(this).find('img').css('opacity', '1');
+    counter++;
+
+
+    if (counter === 2) {
+        counter = 0;
+        setTimeout(hideAgain, 2000);
+    }
 });
 
 // call shuffle function when the page is loaded
