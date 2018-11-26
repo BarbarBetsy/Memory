88d956eb70c17f4f8fa8a01d7cb55bb87655aeb8
BarbarBetsy <barbarbetsy@googlemail.com>
2018-11-26 22:13:21 +0100

    turned cards will be matched

--- js/main.js
+++ js/main.js
@@ -1,28 +1,58 @@
+let counter = 0;
+let turnCounter = 0;
+let pairCounter = 0;
+let cardsTurned = [];
+
 // shuffle the cards by giving each card a random flex-order
 function shuffle() {
     for (i = 1; i <= 16; i++) {
         let num = Math.floor(Math.random() * (100 - 1)) + 1;
         document.querySelector('#card' + i).style.order = num;
     }
-}
+};
 
+// hide cards again
 function hideAgain() {
-$('.card').find('img').css('opacity', '0');
-}
+    $('.front').addClass('back turnable');
+    $('.back').removeClass('front').addClass('turnable').on('click', showCard);
+    counter = 0;
+    cardsTurned = [];
+};
 
-let counter = 0;
+// show card and lock it down
+function showCard() {
+    $(this).removeClass('back').addClass('front').off('click');
 
-// show card
-$('.card').click(function () {
-    $(this).find('img').css('opacity', '1');
+    // count turned cards
+    cardsTurned.push($(this).find("img").attr("src"));
+    console.log(cardsTurned);
     counter++;
 
-
+    // make sure only two cards are turned at the same time
     if (counter === 2) {
-        counter = 0;
-        setTimeout(hideAgain, 2000);
+        $('.turnable').off('click')
+        $('.turnable').removeClass('turnable');
+        turnCounter++;
+
+        // check if cards are the same
+        if (cardsTurned[0] === cardsTurned[1]) {
+            $('.front').addClass('solved').removeClass('front back turnable').off('click');
+            pairCounter++;
+            hideAgain();
+
+        } else {
+            // turn cards again
+            setTimeout(hideAgain, 2000);
+        }
+    };
+
+    if (pairCounter === 8) {
+        console.log('YAY!');
     }
-});
+}
+
+// show card
+$('.turnable').on('click', showCard);
 
 // call shuffle function when the page is loaded
 window.addEventListener('load', shuffle);
